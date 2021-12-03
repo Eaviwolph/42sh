a775712d1e38ebeedc6216745aca44a0c4c8634a
Escudier Quentin <quentin.escudier@epita.fr>
2021-12-03 14:33:33 +0100

    par pitié stop

--- 42sh/src/exectree/exectree.c
+++ 42sh/src/exectree/exectree.c
@@ -2,36 +2,39 @@
 
 void exectree(struct node *n, struct shell *s)
 {
-    if (n->type == CMD)
+    if (n)
     {
-        execcmd(n->data.cmdnode, s);
-    }
-    else if (n->type == AND)
-    {
-        execand(n->data.andnode, s);
-    }
-    else if (n->type == OR)
-    {
-        execor(n->data.ornode, s);
-    }
-    else if (n->type == BANG)
-    {
-        execbang(n->data.bangnode, s);
-    }
-    else if (n->type == PIPE)
-    {
-        exec_pipe(n->data.pipenode, s);
-    }
-    else if (n->type == IF)
-    {
-        execif(n->data.ifnode, s);
-    }
-    else if (n->type == FOR)
-    {
-        execfor(n->data.fornode, s);
-    }
-    else if (n->type == WHILE)
-    {
-        execwhile(n->data.whilenode, s);
+        if (n->type == CMD)
+        {
+            execcmd(n->data.cmdnode, s);
+        }
+        else if (n->type == AND)
+        {
+            execand(n->data.andnode, s);
+        }
+        else if (n->type == OR)
+        {
+            execor(n->data.ornode, s);
+        }
+        else if (n->type == BANG)
+        {
+            execbang(n->data.bangnode, s);
+        }
+        else if (n->type == PIPE)
+        {
+            exec_pipe(n->data.pipenode, s);
+        }
+        else if (n->type == IF)
+        {
+            execif(n->data.ifnode, s);
+        }
+        else if (n->type == FOR)
+        {
+            execfor(n->data.fornode, s);
+        }
+        else if (n->type == WHILE)
+        {
+            execwhile(n->data.whilenode, s);
+        }
     }
 }
